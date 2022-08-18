2a43acabd3a7ced25cca9c4c609fbb7e3b2f71a6
Henrique Souza Silva <101906352+Henrique2811@users.noreply.github.com>
2022-08-17 21:37:18 -0300

    Add files via upload

---
+++ Três mensagens em linhas separadas/Aula01.java
@@ -0,0 +1,9 @@
+// A partir do exemplo visto na aula, desenvolva um programa que coloque na tela três mensagens em linhas separadas. Pode ser seu nome, na primeira linha, “Programa em Java”, na segunda e “Até o próximo programa” na última.
+
+public class Aula01{
+    public static void main(String args[]){
+        System.out.println("Henrique de Souza Silva");
+        System.out.println("Programa em Java");
+        System.out.println("Até o próximo programa");
+    }
+}
